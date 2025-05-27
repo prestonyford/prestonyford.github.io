6affc9ca4a74d15549480a33e2e703c62879a166
Preston Ford <preston.y.ford@gmail.com>
2025-05-26 22:41:49 -0600

    Welcome links

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -8,15 +8,20 @@
 					I'm a Computer Science undergraduate student at BYU. I'm excited at the opportunity
 					to enter the field of Software Engineering.
 				</p>
+				<p>
+					<i>preston.y.ford@gmail.com</i>
+				</p>
 			</div>
 		</div>
 		<div class="flex gap-12">
 			<Link text="Download Resume" href="https://drive.google.com/file/d/1J7vuH9CaMzvhEX4PcgUOKD0Fy-D_ZnyJ/view?usp=sharing" target="_blank" />
-			<Link text="Contact Me" />
+			<Link text="Email Me" href="mailto:preston.y.ford@gmail.com" target="_blank" />
+			<Link text="LinkedIn" :icon="linkedinLogo" href="https://www.linkedin.com/in/preston-ford-897761289/" target="_blank" />
 		</div>
 	</div>
 </template>
 
 <script setup>
-import Link from './Link.vue'
+import Link from '@/components/Link.vue'
+import linkedinLogo from '@/assets/linkedin-logo.svg'
 </script>
