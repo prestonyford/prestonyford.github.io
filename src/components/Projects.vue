7b17040c2cc067beebe2c3196d10064c654b3813
Preston Ford <preston.y.ford@gmail.com>
2025-08-24 19:00:24 -0600

    Add Collaborate + redo tech stack

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,11 +1,47 @@
 <template>
 	<div class="">
 		<h1>Projects</h1>
-		<div class="mt-4 flex flex-col gap-8">
+		<div class="mt-4 flex flex-col gap-12">
+			<ProjectItem name="Collaborate" repo="https://github.com/prestonyford/collaborate">
+				<template #description>
+					<p>
+						Collaborate is a collaborative teamwork and project management application designed to help
+						teams organize work efficiently. It offers an intuitive interface for managing projects,
+						columns, and tasks with powerful features like labels, rich text editing, image upload, and
+						project sharing.
+					</p>
+					<TechStack class="my-4" :stack="['React', 'TypeScript', 'TailwindCSS', 'C#', 'ASP.NET']" />
+					<div class="grid grid-cols-1 md:grid-cols-2 gap-2 mt-2">
+						<img
+							src="https://raw.githubusercontent.com/prestonyford/collaborate/refs/heads/main/ss1.png">
+						<img
+							src="https://raw.githubusercontent.com/prestonyford/collaborate/refs/heads/main/ss2.png">
+						<img
+							src="https://raw.githubusercontent.com/prestonyford/collaborate/refs/heads/main/ss3.png">
+						<img
+							src="https://raw.githubusercontent.com/prestonyford/collaborate/refs/heads/main/ss4.png">
+					</div>
+				</template>
+			</ProjectItem>
+			<ProjectItem name="WPF Music App" repo="https://github.com/prestonyford/AudioPlayerWPF">
+				<template #description>
+					<p>An audio player built with .NET's WPF (Windows Presentation Foundation) framework that provides several useful features, such as
+						editing tag properties (including album art), creating playlists, creating bookmarks, managing
+						custom starting and ending song positions, and volume control on a per-song basis.</p>
+					<TechStack class="my-4" :stack="['C#', '.NET', 'WPF']" />
+				</template>
+				<template #preview>
+					<div class="sm:mt-[2em] w-[246px]">
+						<img
+							src="https://raw.githubusercontent.com/prestonyford/AudioPlayerWPF/refs/heads/main/screenshot1.png">
+					</div>
+				</template>
+			</ProjectItem>
 			<ProjectItem name="Minesweeper" repo="https://github.com/prestonyford/WinXPMinesweeper">
 				<template #description>
-					<p>Minesweeper in the style of Windows XP that runs in the browser. Try it in the demo to the right!</p>
-					<p class="mt-4"><b>Tech stack:</b> Vue.js, TypeScript, Tailwind CSS</p>
+					<p>Minesweeper in the style of Windows XP that runs in the browser. Try it in the demo to the right!
+					</p>
+					<TechStack class="my-4" :stack="['Vue.js', 'TypeScript', 'TailwindCSS']" />
 				</template>
 				<template #preview>
 					<div class="flex flex-col items-center">
@@ -18,27 +54,17 @@
 					</div>
 				</template>
 			</ProjectItem>
-			<ProjectItem name="WPF Music App" repo="https://github.com/prestonyford/AudioPlayerWPF">
-				<template #description>
-					<p>An audio player built with .NET's WPF framework that provides several useful features, such as editing tag properties (including album art), creating playlists, creating bookmarks, managing custom starting and ending song positions, and volume control on a per-song basis.</p>
-					<p class="mt-4"><b>Tech stack:</b> C#, .NET, WPF (Windows Presentation Foundation)</p>
-				</template>
-				<template #preview>
-					<div class="sm:mt-[2em] w-[246px]">
-						<img src="https://raw.githubusercontent.com/prestonyford/AudioPlayerWPF/refs/heads/main/screenshot1.png">
-					</div>
-				</template>
-			</ProjectItem>
 			<ProjectItem name="HTTP Cache Parser" repo="https://github.com/prestonyford/http-cache-parser">
 				<template #description>
-					<p>Inspect the HTTP cache of browsers or online game clients. Searches for media based on their file 
-						signatures and strips out other data that would make the file invalid (such as HTTP headers). 
+					<p>Inspect the HTTP cache of browsers or online game clients. Searches for media based on their file
+						signatures and strips out other data that would make the file invalid (such as HTTP headers).
 						Preview image or audio assets.</p>
-					<p class="mt-4"><b>Tech stack:</b> Node.js, Express.js</p>
+					<TechStack class="my-4" :stack="['Node.js', 'Express.js']" />
 				</template>
 				<template #preview>
 					<div class="sm:mt-[2em] w-[246px]">
-						<img src="https://raw.githubusercontent.com/prestonyford/http-cache-parser/refs/heads/master/screenshot2.png">
+						<img
+							src="https://raw.githubusercontent.com/prestonyford/http-cache-parser/refs/heads/master/screenshot2.png">
 					</div>
 				</template>
 			</ProjectItem>
@@ -49,4 +75,5 @@
 <script setup>
 import ProjectItem from './ProjectItem.vue';
 import BouncyArrow from './BouncyArrow.vue';
+import TechStack from './TechStack.vue';
 </script>
---
+++ src/components/TechStack.vue
@@ -0,0 +1,42 @@
+<template>
+	<div>
+		<p><b>Tech stack:</b></p>
+		<div class="flex flex-wrap gap-2">
+			<div v-for="tech in props.stack" class="flex flex-col items-center min-w-[48px]">
+				<img :src="techImages[tech]" class="h-[32px]"></img>
+				<div>{{ tech }}</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script setup lang="ts">
+import jsLogo from '@/assets/js-logo.png'
+import tsLogo from '@/assets/ts-logo.png'
+import vueLogo from '@/assets/vue-logo.svg'
+import reactLogo from '@/assets/react-logo.png'
+import tailwindLogo from '@/assets/tailwind-logo.svg'
+import javaLogo from '@/assets/java-logo.png'
+import csharpLogo from '@/assets/csharp-logo.png'
+import mysql from '@/assets/mysql-logo.png'
+
+type Tech = 'React' | 'Vue.js' | 'TypeScript' | 'JavaScript' | 'TailwindCSS' | 'C#' | '.NET' | 'ASP.NET' | 'WPF' | 'Node.js' | 'Express.js'
+const techImages: Record<Tech, string> = {
+  React: reactLogo,
+  'Vue.js': vueLogo,
+  JavaScript: jsLogo,
+  TypeScript: tsLogo,
+  TailwindCSS: tailwindLogo,
+  'C#': csharpLogo,
+  '.NET': 'https://upload.wikimedia.org/wikipedia/commons/0/0e/Microsoft_.NET_logo.png',
+  'ASP.NET': 'https://upload.wikimedia.org/wikipedia/commons/0/0e/Microsoft_.NET_logo.png',
+  'WPF': 'https://upload.wikimedia.org/wikipedia/commons/0/0e/Microsoft_.NET_logo.png',
+  'Node.js': 'https://upload.wikimedia.org/wikipedia/commons/d/d9/Node.js_logo.svg',
+  'Express.js': 'https://upload.wikimedia.org/wikipedia/commons/6/64/Expressjs.png'
+}
+
+const props = defineProps<{
+	stack: Tech[]
+}>();
+
+</script>
